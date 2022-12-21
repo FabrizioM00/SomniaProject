9daead74cd3667de8d847e9ddae667c64762816a
FabrizioM00 <116150549+FabrizioM00@users.noreply.github.com>
2022-12-21 16:01:52 +0100

    commit

---
+++ SomniaBE/src/main/java/it/somnia/security/JWTSecurityConfig.java
@@ -0,0 +1,50 @@
+package it.somnia.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+//@EnableWebSecurity
+//@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class JWTSecurityConfig {
+	
+	@Autowired
+	private JWTRequestFilter jwtRequestFilter;
+	
+	@Bean
+    public AuthenticationManager authenticationManager(
+            final AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        return http
+        		.cors().and()
+                .csrf().disable()
+                .authorizeRequests()
+                        .antMatchers("api/auth/signup").permitAll() //accesso senza autenticazione
+                        .antMatchers("api/auth/login").permitAll()
+                        .antMatchers("user/api", "/user/api**").hasRole("USER")
+                        .antMatchers("admin/api", "/admin/api/**").hasRole("ADMIN").and()
+                        //anyRequest().authenticated().and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+}
+
